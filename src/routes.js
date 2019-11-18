341d640e172a2f1e5b428b4e4e0887468faf9f28
Guibson Arcebispo <guibsonarc@gmail.com>
2019-11-18 00:19:55 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Guibson Arcebispo',
-    email: 'guibsonarc@gmail.com',
-    password_hash: '214fwtgt3t34t22341313f',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
