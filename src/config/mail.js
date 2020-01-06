48f9ac943b602cd16d6be3e6ffe4f9f4fca0d357
Guibson Arcebispo <guibsonarc@gmail.com>
2020-01-06 00:40:12 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'a5dea9d69a0d4a',
+    pass: '7eaa43b50d4584',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
